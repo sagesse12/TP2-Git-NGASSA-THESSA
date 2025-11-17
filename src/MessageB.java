0b7d1ff3564002d84e71aaa971904f658b26532e
sagesse12 <sthessaa@gmail.com>
2025-11-17 11:40:50 +0100

    Ajout d'une deuxieme ligne de A

--- src/MessageB.java
+++ src/MessageB.java
@@ -4,6 +4,7 @@ public class MessageB {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("message du membre B");
+		System.out.println("deuxieme ligne de A");
 
 	}
 
